1ee359084b419e9c001522e4172721eba0bbbf40
eduardo38oitao <klen.soares@escola.pr.gov.br>
2026-04-08 11:53:48 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,41 @@
+//EDUARDO KLEN - 08/04/2026
+// 3 tds
+
+//1 1. exiba a mensagem "ola mundo" no console.
+ 
+console.log("ola mundo");
+
+//2 crie uma variavel com seu nome e exiba o valor no console.
+
+
+let nome = "Eduardo Klen Soares";
+console.log(nome);
+
+//3.some dois numeros e exiba o resultados
+
+let numero = 1+1;
+console.log(numero);
+
+
+//4.subtrai dois numeros e exiba o valor
+let a = 1-1;
+console.log(a);
+
+
+//5. descubra o resto da divisão de um numero por outro
+
+let b = 5%5;
+console.log(b);
+
+//6. crie uma variavel booleana(true e false) e exiba o valor
+let c = true;
+let d = false;
+console.log(c,d);
+
+//7. verifique se um numero é maior que outro
+
+
+let verificacao= 1> 2;
+console.log(verificacao);
+
+//8.
