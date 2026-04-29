a378b4d6e15dd665041d2dbe765d870da6b83fa1
eduardo38oitao <klen.soares@escola.pr.gov.br>
2026-04-29 11:49:27 -0300

    Add files via upload

--- teste.js
+++ teste.js
@@ -422,6 +422,77 @@ console.log(`normal: ${matriz2x}`);
 console.log(`multiplado: ${valorrr}`);
 
 
+//foreach
+console.log("Exercício 1: Mostrar Elementos\n");
+//Essas chaves servem para abrir e fechar escopo.
+{
+let numeros = [1,2,3,4];
+console.log("Usando o for:");
+for(let i = 0; i < numeros.length; i ++){
+    console.log(numeros[i]);
+}
+console.log("Usando o forEach:")
+numeros.forEach(function(n){
+    if(n % 2 == 0){
+    console.log(n);
+    }
+    
+});
+
+console.log("for Each arrow (modo seta):");
+numeros.forEach(n => console.log(n));
+}
+// O forEach percorre o arreio, o que foi percorrido vira o n.
+
+
+//mostre todos os valores no terminal
+//dados do array
+// let frutas ["maça","banana","uva","abacate"]
+//ultilizar foreach função anonima e função arrow
+console.log("Exercício 1: Mostrar Elementos\n");
+//Essas chaves servem para abrir e fechar escopo.
+{
+let frutata = ["maça","banana","uva","abacate"];
+console.log("Usando o forEach:")
+frutata.forEach(function(n){
+    
+    console.log(n);
+    
+    
+});
+
+console.log("for Each arrow (modo seta):");
+frutata.forEach(n => console.log(n));
+}
+// O forEach percorre o arreio, o que foi percorrido vira o n.
+
+
+
+
+//some todos os valores mostre os resultados
+//preços 10,30,50,60:
+
+{
+let preco = [10,30,50,60];
+let vazio = 0
+preco.forEach(function(n){
+    vazio = vazio+n
+    console.log(vazio)
+
+
+});
+
+preco.forEach(n => console.log(n))
+
+
+
+}
+
+
+
+
+
+
 
 ////////////////// objetos ////////////////////////
 
